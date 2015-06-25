716b918631fefcb894bce6e0ba9defd5f6d6d2d9
owlg <owlg@users.noreply.github.com>
2015-06-25 19:12:02 +0800

    Update server.js
    Heroku support added, with major changes

--- main/server.js
+++ main/server.js
@@ -5,15 +5,49 @@ var mime = require("mime");
 
 
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
-}).listen(3000);
 
-
+// send 404 if content doesn't exist
 function send404(response) {
   response.writeHead(404, {"Content-type" : "text/plain"});
   response.write("Error 404: resource not found");
   response.end();
 }
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+//handle responses, no content to redirect to 404 Not found
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+
+//server creation 
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+//Heroku port compatibility
+var port_number = server.listen(process.env.PORT || 3000);
